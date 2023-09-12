41cedd2e14b97648f4872345c92e8b6d2abbc9c3
Kyle Washington <kmw1993@msn.com>
2023-09-12 14:38:59 -0400

    Adding background color to my webpage. Also added code to my newpostform

---
+++ src/Components/NewPostForm/NewPostForm.jsx
@@ -0,0 +1,40 @@
+const NewPostForm = ({onAddPost}) => {
+    const [newPost, setNewPost] = useState({name: '', body: ''});
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setNewPost({...newPost, [name]: value });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onAddPost(newPost);
+        setNewPost({name: '', body: ''});
+    };
+
+    return ( 
+        <div>
+            <h2>Add New Post</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input 
+                    type="text"
+                    name="name"
+                    value={newPost.name}
+                    onChange={handleChange}/>
+                </label>
+                <br/>
+                <label>
+                    Body:
+                    <textarea 
+                    name="body"
+                    value={newPost.body}
+                    onChange={handleChange}/>
+                </label>
+                <br/>
+                <button type="submit">Add Post</button>
+            </form>
+        </div>
+     );
+};
+ 
+export default NewPostForm;
